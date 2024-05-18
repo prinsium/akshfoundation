6a66e645bf3120839febd585fc2eea7645b2c8fe
3vdi <unitcomics.world@proton.me>
2024-05-18 21:24:51 +0530

    18 may 2

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://akshfoundation.com',
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 1,
+      },
+    ]
+  }
